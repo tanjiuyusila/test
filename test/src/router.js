ec4c9126f5af8cb3dbcdedafece4c78fb163bead
tanjiuyusila <521196748@qq,com>
2019-07-16 20:02:40 +0800

    test

--- test/src/router.js
+++ test/src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Test from './views/Test.vue';
+import SetTest from './views/SetTest.vue';
+import SingleTest from './views/SingleTest.vue';
 
 Vue.use(Router);
 
@@ -14,12 +17,22 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/test',
+      name: 'test',
+      component: Test,
+      children:[
+        {
+          path: 'setTest',
+          name: 'setTest',
+          component: SetTest
+        },
+        {
+          path: 'singleTest',
+          name: 'singleTest',
+          component: SingleTest
+        },
+      ]
     },
+
   ],
 });
---
+++ test/src/views/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/test/setTest">SetTest</router-link>|
+    <router-link to="/test/singleTest">SingleTest</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
