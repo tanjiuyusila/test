eb5ceca51ff336c6309f138f8be40cb7c2e9176b
tanjiuyusila <521196748@qq,com>
2019-07-18 20:37:24 +0800

    test

--- test/src/components/AnswerArea.vue
+++ test/src/components/AnswerArea.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <h1>{{nowIndex}}.编程题</h1>
-
+    <iframe src="http://localhost:8080/code" width="500px" height="500px" frameborder="0" name="progranIframe" scrolling="yes">
+    </iframe>
   </div>
 </template>
 
 <script>
+  var a = document.getElementsByTagName('iframe');
+  console.log(a);
   export default {
     created(){
       this.nowTest = this.program[0];
       this.programNum = this.program.length;
-      // console.log(this.nowTest);
+
     },
     data(){
       return {
