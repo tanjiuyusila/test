ec98573207dbf31293e89914f06c3c78bd4fd27c
tanjiuyusila <521196748@qq,com>
2019-07-18 23:30:16 +0800

    test

--- e_test/models/db.js
+++ e_test/models/db.js
@@ -4,7 +4,7 @@ var pool  = mysql.createPool({
   host            : 'localhost',
   user            : 'root',
   password        : '',
-  database        : 'muke'
+  database        : 'exam'
 });
 
 exports.query = function(sql,params,callback){
