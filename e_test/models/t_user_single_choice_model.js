cc08d6b0fe5da2096d32a455e170ffe5a9ea2d2a
kaixinjiuhaomyx <1416191825@qq.com>
2019-07-19 23:57:11 +0800

    aaa

--- e_test/models/t_user_single_choice_model.js
+++ e_test/models/t_user_single_choice_model.js
@@ -1,6 +1,2 @@
 var db = require("./db.js");
 
-exports.s_m_write = function(tocken,sc,answer,date_s,callback){
-    var sql = "insert into t_user_single_choice(token_id,sc_id,user_answer,commit_date) values(?,?,?,?) ";
-    db.query(sql,[tocken,sc,answer,date_s],callback);
-}
