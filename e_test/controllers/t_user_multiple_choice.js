394749d2be90ad0bcc29f2c8a6090f7af6f581f5
kaixinjiuhaomyx <1416191825@qq.com>
2019-07-19 15:17:49 +0800

    写入

--- e_test/controllers/t_user_multiple_choice.js
+++ e_test/controllers/t_user_multiple_choice.js
@@ -1,14 +1,25 @@
 var t_User_multiple_choice_model = require('../models/t_User_multiple_choice_model.js');
 
 
-exports.m_write = function(req,res,next){
-    var umc = req.body.umc_id;
+// exports.m_write = function(req,res,next){
+//     var umc = req.body.umc_id;
+//     var token = req.body.token_id;
+//     var mc = req.body.mc_id;
+//     var answer = req.body.user_answer;
+//     var date = req.body.commit_date;
+    
+//      t_User_multiple_choice_model.m_m_write(umc,token,mc,answer,date,function(err,data){
+//          console.log(data);
+//      })
+// }
+
+exports.all_m_write = function(req,res,next){
     var token = req.body.token_id;
     var mc = req.body.mc_id;
-    var answer = req.body.user_answer;
-    var date = req.body.commit_date;
+    var answer_m = req.body.user_m_answer;
+    var date_m = req.body.commit_m_date;
     
-     t_User_multiple_choice_model.m_m_write(umc,token,mc,answer,date,function(err,data){
-         console.log(data);
-     })
+    t_User_multiple_choice_model.m_m_write(token,mc,answer_m,date_m,function(err,data){
+        console.log(data);
+    })
 }
